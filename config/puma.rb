f5eb72f63ea1c57f737a94c813d722bc4a106f54
tonycarballeira <tonycarballeira@gmail.com>
2023-04-01 13:19:43 -0400

    render config

--- config/puma.rb
+++ config/puma.rb
@@ -15,8 +15,8 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-bind 'tcp://0.0.0.0:3000'
-# port ENV.fetch("PORT") { 3000 }
+# bind 'tcp://0.0.0.0:3000'
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -25,6 +25,8 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
@@ -38,7 +40,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
